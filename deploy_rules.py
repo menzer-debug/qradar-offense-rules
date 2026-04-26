4205eddb931221cec0ea7b59b1f26d3b79e3ab0d
menzer-debug <menzerqwe@gmail.com>
2026-04-26 15:33:11 +0200

    deploy_rules.py

--- deploy_rules.py
+++ deploy_rules.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import json
+
 QRADAR_IP = os.getenv('QRADAR_CONSOLE_IP')
 TOKEN = os.getenv('QRADAR_SEC_TOKEN')
 
@@ -16,26 +17,26 @@ def upload_rule(rule_file):
         with open(rule_file, 'r') as f:
             rule_data = json.load(f)
 
-
-url = f"https://{QRADAR_IP}/api/analytics/custom_rules"
+        url = f"https://{QRADAR_IP}/api/analytics/rules"
 
         response = requests.post(
             url,
             headers=headers,
             json=rule_data,
-            verify=False  # Lab mühiti üçün
+            verify=False
         )
+
+        print(response.status_code)
+        print(response.text)
 
         if response.status_code in [200, 201, 202]:
             print(f"[SUCCESS] {rule_file} yükləndi")
         else:
             print(f"[ERROR] {rule_file} → {response.status_code}")
-            print(response.text)
 
     except Exception as e:
         print(f"[EXCEPTION] {rule_file} → {str(e)}")
 
-# bütün rule-ları upload et
 for file in os.listdir('rules'):
     if file.endswith('.json'):
         upload_rule(f'rules/{file}')
